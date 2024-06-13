abe39d088c4dd6c786f96b493c82464feb818c8e
Mitul Shesh <mitul.shesh28@gmail.com>
2024-06-13 09:09:19 +0530

    binary subarray sum fixed

--- Data_Structures_and_Algorithms/src/SlidingWindowTwoPointer/CountSubarraysWithSumk.java
+++ Data_Structures_and_Algorithms/src/SlidingWindowTwoPointer/CountSubarraysWithSumk.java
@@ -1,6 +1,6 @@
 package SlidingWindowTwoPointer;
 
-//given its binary array
+//given its binary array count subarrays with sum = k
 /*
 
 Explanation - count number of subarrays with sum < = k and then subtract it with number of subarrays with sum < = k- 1
@@ -13,8 +13,8 @@ public class CountSubarraysWithSumk {
     public static void main(String[] args) {
         int arr[] = { 0,1,1,1,1};
         int goal = 3;
-        System.out.println(logic(arr,goal));
-        //System.out.println(logic(arr,goal) - logic(arr,goal-1));
+        //System.out.println(logic(arr,goal));
+        System.out.println(logic(arr,goal) - logic(arr,goal-1));
     }
 
     public static int logic(int[] arr,int goal){
@@ -33,14 +33,11 @@ public class CountSubarraysWithSumk {
 
             sum+=arr[r];
 
-            if(sum <= goal){
-                count = count + (r - l + 1);
-            }
-
             while(sum > goal){
                 sum = sum - arr[l];
                 l++;
             }
+            count = count + (r - l + 1);
 
             r++;
         }
